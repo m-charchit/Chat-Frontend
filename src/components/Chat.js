5e1e7b5d5f53cb44e42bd86863f18b1404e92a7a
charchit <charchit.dahiya@gmail.com>
2022-06-21 00:38:23 +0530

    chat styling done

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React from "react";
 
 function Chat() {
   return (
     <div className="w-full h-screen sm:h-[480px] sm:w-[400px] mx-auto shadow-[0_0px_10px_rgba(0,0,0,0.253)] transition-all sm:rounded-lg">
-        <div className="flex flex-col  h-full">
+      <div className="flex flex-col  h-full">
         <div className="flex justify-between items-center border-b px-3 py-2 shadow">
-        <div className="flex space-x-2 items-center">
-        <i class="fas fa-arrow-left"></i>
+          <div className="flex space-x-2 items-center">
+            <i class="fas fa-arrow-left"></i>
             <img src="/favicon.ico" alt="" className="rounded-full w-12 h-12" />
             <div>
               <p className="font-bold text-2xl">Charchit</p>
               Last Seen Oct 16 20:22
             </div>
           </div>
-</div>
-<div className="h-full shadow"></div>
-<div className="flex ">
-<input type="text" className="px-3 py-2 outline-0 border-[1px]  border-[#ced4da] focus:border-blue-500 w-full shadow" placeholder="Message..." />
-    <button className='bg-black px-2 text-white'>Send</button>
-</div>
         </div>
+        <div className="flex flex-col h-full bg-gray-100 overflow-auto px-3 font-['Poppins', sans-serif]">
+          <div className="flex space-x-2 my-4">
+            <img src="/favicon.ico" alt="" className="rounded-full w-8 h-8" />
+            <div className="flex flex-col px-3 py-2.5 rounded-r-2xl rounded-b-2xl bg-black max-w-[75%]  text-white hover:shadow-lg shadow-md transition-shadow duration-500">
+              <div className="flex justify-between space-x-3">
+                <span className="">Lorem ipsum dolor sit amet.</span>
+                <i className="fa-solid fa-angle-down text-xs cursor-pointer"></i>
+              </div>
+              <p className="text-right font-semibold text-[10px]">Oct 14 22:03</p>
+            </div>
+          </div>
+          <div className="space-x-2 my-4">
+            <div className="flex max-w-[80%] float-right flex-col px-3 py-2.5 rounded-l-2xl rounded-b-2xl bg-white hover:shadow-lg shadow-md transition-shadow duration-500  ">
+              <div className="flex justify-between space-x-3">
+                <span className="">Lorem, ipsum dolor.lorem Lorem, ipsum dolor. lorem31</span>
+                <i className="fa-solid fa-angle-down text-xs cursor-pointer"></i>
+              </div>
+              <p className="text-right font-semibold text-[10px]">Oct 14 22:03</p>
+            </div>
+          </div>
         </div>
-  )
+        <div className="flex ">
+          <input
+            class="text-gray-700 shadow border border-gray-300 focus:outline-none focus:shadow-outline py-2 px-3 w-full"
+            type="text"
+            placeholder="Message..."
+          />
+          <button className="bg-black px-3 text-white">
+            <i class="fa-solid fa-paper-plane"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
--- src/components/Home.js
+++ src/components/Home.js
@@ -30,26 +30,26 @@ function Home() {
 
               <div class="dot absolute w-6 h-6 bg-white rounded-full peer-checked:bg-green-600 peer-checked:translate-x-full shadow -left-1 -top-3 transition"></div>
             </div>
-              <div class="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
-
+            <div class="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
           </label>
         </div>
         <hr />
         <div className="flex py-2">
-        <input type="text" className="px-3 py-2 outline-0 border-[1px]  border-[#ced4da] focus:border-blue-500 rounded-md w-full" placeholder="Enter Name to Search" />
-        </div>  
+        <input class="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-2 px-3 w-full" type="text" placeholder="Enter Name to Search"/>
+        </div>
         <div className="flex flex-col h-full overflow-auto">
-        <div className="flex justify-between items-center hover:bg-gray-100 cursor-pointer transition-colors py-2 last:border-b-0 border-b">
-          <div className="flex space-x-2">
-            <img src="/favicon.ico" alt="" className="rounded-full w-12" />
-            <div>
-              <p className="font-bold">Charchit</p>
-              <span className="font-extralight text-gray-600">Hey There</span>
+          <div className="flex justify-between items-center hover:bg-gray-100 cursor-pointer transition-colors py-2 last:border-b-0 border-b">
+            <div className="flex space-x-2">
+              <img src="/favicon.ico" alt="" className="rounded-full w-12" />
+              <div>
+                <p className="font-bold">Charchit</p>
+                <span className="font-extralight text-gray-600">Hey There</span>
+              </div>
+            </div>
+            <div className="rounded-full bg-blue-600 text-white px-2.5 py-0.5">
+              1
             </div>
           </div>
-          <div className="rounded-full bg-blue-600 text-white px-2.5 py-0.5">1</div>
-        </div>
-          
         </div>
       </div>
     </div>
